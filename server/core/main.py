9f0d8641c8f95ec99f7fedde81663bd52199c9a6
KanArtur <2007kanart@gmail.com>
2023-03-19 13:23:55 +0300

    second

---
+++ server/core/main.py
@@ -0,0 +1,27 @@
+from flask import Flask
+import os
+import json
+
+
+app = Flask("server")
+DEFAULT_CONFIG = {"port": 5555, "debug": True}
+
+__dir__ = os.path.abspath(os.path.dirname(__file__))
+data_dir = f"{__dir__}/data"
+if not os.path.isdir(data_dir):
+    os.mkdir(data_dir)
+
+
+def read_config():
+    home_dir = os.path.dirname(__file__)
+    cfg_file = os.path.join(home_dir, "config.json")
+    config = {**DEFAULT_CONFIG}
+    try:
+        with open(cfg_file) as f:
+            data = json.load(f)
+        config["port"] = data.get("port", DEFAULT_CONFIG["port"])
+        config["debug"] = data.get("debug", DEFAULT_CONFIG["debug"])
+    except (FileNotFoundError, json.JSONDecodeError):
+        with open(cfg_file, "w") as fp:
+            json.dump(DEFAULT_CONFIG, fp)
+    return config
